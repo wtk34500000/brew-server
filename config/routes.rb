b82ff12d09229a61348d5544681b906c72b26040
Tony_Huang <luck938938@hotmail.com>
2019-07-24 16:52:46 -0400

    change method name to get_breweries

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # resources :breweries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  post '/breweries', to: 'breweries#index'
+  post '/breweries', to: 'breweries#get_breweries'
  
 end
