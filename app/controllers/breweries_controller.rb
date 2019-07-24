d90d3872a8cfacc15e31e384e5d1038581bb80d8
Tony_Huang <luck938938@hotmail.com>
2019-07-24 17:02:11 -0400

    change name

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -2,7 +2,7 @@ require 'rest-client';
 
 class BreweriesController < ApplicationController
 
-    def index
+    def get_breweries
         term=params[:term]
         url="https://api.openbrewerydb.org/breweries?by_city=#{term}"
         @res = RestClient.get(url)
