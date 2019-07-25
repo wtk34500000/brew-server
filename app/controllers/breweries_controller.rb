f8baba7296bf88cef3956b5bb60802eb6bb0ceae
Tony_Huang <luck938938@hotmail.com>
2019-07-24 22:20:55 -0400

    change per page to 50 intead of 20 by default

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,7 +4,7 @@ class BreweriesController < ApplicationController
 
     def get_breweries
         term=params[:term]
-        url="https://api.openbrewerydb.org/breweries?by_city=#{term}"
+        url="https://api.openbrewerydb.org/breweries?by_city=#{term}&per_page=50"
         @res = RestClient.get(url)
         render json: @res
     end
